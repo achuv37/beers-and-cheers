ab3d798a0a4f90ec97d01ed3a430ba9e88b90044
beatricekallen <bkallen90@gmail.com>
2022-03-26 12:39:05 -0400

    adds local storage functionality, adds styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var cityInputEl = document.querySelector("#city-input");
 var eventsContainerEl = document.querySelector("#events");
 var breweriesContainerEl = document.querySelector("#breweries");
 var submitButtonEl = document.querySelector("#button");
+var searchHistoryContainerEl = document.querySelector("#recent-searches");
 
 // form handler function: needs to prevent default and run getEvents and getBreweries
 var formSubmitHandler = function (event) {
@@ -18,6 +19,9 @@ var formSubmitHandler = function (event) {
 
   getEvents(city);
   getBreweries(city);
+
+  storeCities(city);
+  loadCities();
 };
 
 // getEvents function: return info from Ticketmaster
@@ -63,13 +67,16 @@ var getBreweries = function (city) {
 // displayBreweries: displays brewery information to the page
 var displayBreweries = function (data) {
   breweriesContainerEl.textContent = "";
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 9; i++) {
     console.log("working");
 
     var breweryCard = document.createElement("div");
-    breweryCard.setAttribute("class", "card");
+    breweryCard.setAttribute("class", "card m-3 has-background-link-dark");
     var breweryCardTitle = document.createElement("h2");
-    breweryCardTitle.setAttribute("class", "card-header-title");
+    breweryCardTitle.setAttribute(
+      "class",
+      "has-text-weight-semibold has-text-centered is-size-3 has-text-white"
+    );
     breweryCardTitle.textContent = data[i].name;
 
     var breweryCardStreet = document.createElement("p");
@@ -77,8 +84,11 @@ var displayBreweries = function (data) {
 
     breweryCardStreet.textContent = `Address: ${data[i].street}`;
     breweryCardPostalCode.textContent = data[i].postal_code;
-    breweryCardStreet.setAttribute("class", "card-content");
-    breweryCardPostalCode.setAttribute("class", "card-content");
+    breweryCardStreet.setAttribute("class", "is-size-4 m-2 p-2 has-text-white");
+    breweryCardPostalCode.setAttribute(
+      "class",
+      "is-size-4 m-2 p-2 has-text-white"
+    );
 
     breweryCard.appendChild(breweryCardTitle);
     breweryCard.appendChild(breweryCardStreet);
@@ -95,45 +105,52 @@ var displayEvents = function (data) {
     console.log("working");
 
     var eventCard = document.createElement("div");
-    eventCard.setAttribute("class", "card");
+    eventCard.setAttribute("class", "card m-2 has-background-link-dark");
     var eventCardTitle = document.createElement("h2");
-    eventCardTitle.setAttribute("class", "card-header-title");
+    eventCardTitle.setAttribute(
+      "class",
+      "has-text-weight-semibold has-text-centered is-size-3 has-text-white"
+    );
     eventCardTitle.textContent = data._embedded.events[i].name;
 
     var eventCardStartDate = document.createElement("p");
     var eventCardStartTime = document.createElement("p");
-    var eventCardPriceMin = document.createElement("p");
-    var eventCardPriceMax = document.createElement("p");
-    var eventCardUrl = document.createElement("p");
+    var eventCardUrl = document.createElement("a");
     var eventCardVenue = document.createElement("p");
     var eventCardImg = document.createElement("img");
 
-    eventCardStartDate.setAttribute("class", "card-content");
-    eventCardStartTime.setAttribute("class", "card-content");
-    eventCardPriceMin.setAttribute("class", "card-content");
-    eventCardPriceMax.setAttribute("class", "card-content");
-    eventCardUrl.setAttribute("class", "card-content");
-    eventCardVenue.setAttribute("class", "card-content");
-    eventCardImg.setAttribute("class", "card-image");
-    eventCardImg.setAttribute("class", "card-image image is-128x128 ml-2");
-    eventCardImg.setAttribute("src", data._embedded.events[i].images[i].url)
+    eventCardStartDate.setAttribute(
+      "class",
+      "ml-5 has-text-left is-size-4 has-text-white"
+    );
+    eventCardStartTime.setAttribute(
+      "class",
+      "ml-5 has-text-left is-size-4 has-text-white"
+    );
+    eventCardUrl.setAttribute(
+      "class",
+      "has-text-left is-size-4 m-2 has-text-white"
+    );
+    eventCardVenue.setAttribute(
+      "class",
+      "ml-5 has-text-left is-size-4 has-text-white"
+    );
+    eventCardImg.setAttribute("class", "card-image image is-128x128 ml-4 p-2");
+    eventCardImg.setAttribute("src", data._embedded.events[i].images[i].url);
 
     eventCardStartDate.textContent = `Start date: ${data._embedded.events[i].dates.start.localDate}`;
     eventCardStartTime.textContent = `Start time: ${data._embedded.events[i].dates.start.localTime}`;
-    eventCardPriceMin.textContent = `Lowest price: ${data._embedded.events[i].priceRanges[0].min}`;
-    eventCardPriceMax.textContent = `Highest price: ${data._embedded.events[i].priceRanges[0].max}`;
     eventCardImg.textContent = data._embedded.events[i].images[i].url;
 
     //need to look at image ratio information for embedding in card
-    eventCardUrl.textContent = `For more details: ${data._embedded.events[i].url}`;
+    eventCardUrl.href = data._embedded.events[i].url;
+    eventCardUrl.innerHTML = "For more details click here.";
     eventCardVenue.textContent = `Venue: ${data._embedded.events[i]._embedded.venues[0].name}`;
 
     eventCard.appendChild(eventCardTitle);
     eventCard.appendChild(eventCardImg);
     eventCard.appendChild(eventCardStartDate);
     eventCard.appendChild(eventCardStartTime);
-    eventCard.appendChild(eventCardPriceMin);
-    eventCard.appendChild(eventCardPriceMax);
     eventCard.appendChild(eventCardVenue);
     eventCard.appendChild(eventCardUrl);
     eventsContainerEl.appendChild(eventCard);
@@ -150,23 +167,31 @@ var storeCities = function (city) {
 var loadCities = function () {
   var storedCities = JSON.parse(localStorage.getItem("search-history"));
 
+  searchHistoryContainerEl.textContent = "";
+
   for (var i = 0; i < storedCities.length; i++) {
+    var searchHistoryButtonEl = document.createElement("button");
+    searchHistoryButtonEl.textContent = storedCities[i];
     searchHistoryButtonEl.setAttribute("data-search", storedCities[i]);
+    searchHistoryButtonEl.setAttribute("type", "submit");
+    searchHistoryButtonEl.setAttribute(
+      "class",
+      "button is-link is-outlined is-rounded mx-3"
+    );
+
+    searchHistoryContainerEl.appendChild(searchHistoryButtonEl);
   }
 };
 
-//   $(document).ready(loadCities);
+$(document).ready(loadCities);
 
 //event listener for submit form
 submitButtonEl.addEventListener("click", formSubmitHandler);
 
 //event listener for search history buttons
-// document
-//   .querySelector("#search-history")
-//   .addEventListener("click", function (event) {
-//     event.preventDefault();
-//     var city = event.target.dataset.search;
-//     getBreweries(city);
-//     getEvents(city);
-//   });
-// comments.
+searchHistoryContainerEl.addEventListener("click", function (event) {
+  event.preventDefault();
+  var city = event.target.dataset.search;
+  getBreweries(city);
+  getEvents(city);
+});
